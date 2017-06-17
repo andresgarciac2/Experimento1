34975a77551752d3923d13e9294e5acf6e20980e
Javier <jh.mesa@uniandes.edu.co>
2017-06-17 11:49:34 -0500

    Creación DTO

--- src/main/java/co/com/uniandes/arquitectura/paciente/controller/PacienteController.java
+++ src/main/java/co/com/uniandes/arquitectura/paciente/controller/PacienteController.java
@@ -1,13 +1,16 @@
 package co.com.uniandes.arquitectura.paciente.controller;
 
 import co.com.uniandes.arquitectura.controller.Controller;
+import co.com.uniandes.arquitectura.persistence.EpisodioDTO;
 import io.vertx.rxjava.ext.web.RoutingContext;
 
 public class PacienteController implements Controller {
 
 	public void crearEpisodio(RoutingContext ctx) {
-		String request = ctx.getBodyAsString();
-	    respondWithJson(ctx, 200, request + "Episodio Creado");
+		//String request = ctx.getBodyAsString();
+		EpisodioDTO req = extractBodyAsJson(ctx, EpisodioDTO.class);
+		
+	    respondWithJson(ctx, 200, "El nombre del paciente es: " + req.getNombre());
 	}
 	
 }
