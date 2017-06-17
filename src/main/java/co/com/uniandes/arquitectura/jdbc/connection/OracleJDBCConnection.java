d13bddbf896ccf959c8f512eefa59432901f18cc
Andres Garcia <andresgarcia@seven4n.com>
2017-06-17 11:53:24 -0500

    se hace conexion a la base de datos

---
+++ src/main/java/co/com/uniandes/arquitectura/jdbc/connection/OracleJDBCConnection.java
@@ -0,0 +1,71 @@
+package co.com.uniandes.arquitectura.jdbc.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public final class  OracleJDBCConnection {
+	
+    public Connection conn;
+    private Statement statement;
+    public static OracleJDBCConnection db;
+    private OracleJDBCConnection() {
+        String url= "jdbc:oracle:thin:@localhost:1521:xe";
+        String driver = "oracle.jdbc.driver.OracleDriver";
+        String userName = "svebanistmo";
+        String password = "burra24";
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection)DriverManager.getConnection(url,userName,password);
+        }
+        catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+    /**
+     *
+     * @return MysqlConnect Database connection object
+     */
+    public static synchronized OracleJDBCConnection getDbCon() {
+        if ( db == null ) {
+            db = new OracleJDBCConnection();
+        }
+        return db;
+ 
+    }
+    /**
+     *
+     * @param query String The query to be executed
+     * @return a ResultSet object containing the results or null if not available
+     * @throws SQLException
+     */
+    public ResultSet query(String query) throws SQLException{
+        statement = db.conn.createStatement();
+        ResultSet res = statement.executeQuery(query);
+        return res;
+    }
+    /**
+     * @desc Method to insert data to a table
+     * @param insertQuery String The Insert query
+     * @return boolean
+     * @throws SQLException
+     */
+    public int insert(String insertQuery){
+        
+        int result = 0;
+		try {
+			statement = db.conn.createStatement();
+			result = statement.executeUpdate(insertQuery);
+			return result;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return result;
+ 
+    }
+ 
+}
--- src/main/java/co/com/uniandes/arquitectura/paciente/controller/PacienteController.java
+++ src/main/java/co/com/uniandes/arquitectura/paciente/controller/PacienteController.java
@@ -1,12 +1,17 @@
 package co.com.uniandes.arquitectura.paciente.controller;
 
+import java.sql.SQLException;
+
 import co.com.uniandes.arquitectura.controller.Controller;
+import co.com.uniandes.arquitectura.jdbc.connection.OracleJDBCConnection;
 import io.vertx.rxjava.ext.web.RoutingContext;
 
 public class PacienteController implements Controller {
 
 	public void crearEpisodio(RoutingContext ctx) {
 		String request = ctx.getBodyAsString();
+		OracleJDBCConnection conn = OracleJDBCConnection.getDbCon();
+		conn.insert("INSERT INTO EPISODIOS_X_PACIENTE (ID,NOMBRE,NIVELDOLOR) VALUES (11,\'Andres\',\'BAJO\')");
 	    respondWithJson(ctx, 200, request + "Episodio Creado");
 	}
 	
